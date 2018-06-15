8c101c86ef7d8b22ed1c43745c95b97f0d8b94fd
wangfei10 <wangfei10@finupgroup.com>
2018-06-15 18:22:36 +0800

    独立common包

--- example-microframe-order/example-microframe-order-facade/src/main/java/com/example/microframe/order/facade/api/OrderServiceApi.java
+++ example-microframe-order/example-microframe-order-facade/src/main/java/com/example/microframe/order/facade/api/OrderServiceApi.java
@@ -4,6 +4,7 @@ package com.example.microframe.order.facade.api;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
+import com.example.microframe.common
 
 /**
  * 数据中心服务接口
@@ -14,8 +15,8 @@ import java.util.Map;
 @RequestMapping("/remote/dataService")
 public interface OrderServiceApi {
 
-    //@RequestMapping("/getAllDepts")
-    //RemoteResult<Map<Integer, String>> getDeptNameMap();
+    @RequestMapping("/getAllDepts")
+    RemoteResult<Map<Integer, String>> getDeptNameMap();
 
     /**
      * 根据用户id获取订单列表
