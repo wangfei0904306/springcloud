17a03d7be34756be7d8c984fcf7ff40bd2173560
wangfei10 <wangfei10@finupgroup.com>
2018-06-20 22:21:42 +0800

    独立common包

---
+++ example-microframe-common/src/main/java/com/example/microframe/common/result/JsonResult.java
@@ -0,0 +1,134 @@
+package com.example.microframe.common.result;
+
+import java.io.Serializable;
+
+/**
+ * 封装json结果集
+ */
+public class JsonResult<T>  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Boolean success = false;          // 返回是否成功
+    private String msg = "";                  // 返回信提示，可用于前端显示
+    private String detail = "";               // 返回信息详情，保存不便于前端展示的内容
+    private T obj = null;                     // 返回其他对象信息
+
+    public JsonResult() {
+    }
+
+    public JsonResult(Boolean success, String msg) {
+        this.success = success;
+        this.msg = msg;
+    }
+
+    public JsonResult(Boolean success, String msg, T obj) {
+        this.success = success;
+        this.msg = msg;
+        this.obj = obj;
+    }
+
+    public JsonResult(Boolean success, String msg, String detail) {
+        this.success = success;
+        this.msg = msg;
+        this.detail = detail;
+    }
+
+    public JsonResult(Boolean success, String msg, String detail, T obj) {
+        this.success = success;
+        this.msg = msg;
+        this.detail = detail;
+        this.obj = obj;
+    }
+
+    //---------------------------------- 返回成功 -----------------------------------------
+
+    public static <T> JsonResult<T> resultSuccess() {
+        return resultSuccess("操作成功", null);
+    }
+
+    public static <T> JsonResult<T> resultSuccess(T obj) {
+        return resultSuccess("操作成功", obj);
+    }
+
+    public static <T> JsonResult<T> resultSuccess(String message, T obj) {
+        return new JsonResult<T>(true, message, obj);
+    }
+
+    public static <T> JsonResult<T> resultSuccess(String message, String detail, T obj) {
+        return new JsonResult<T>(true, message, detail, obj);
+    }
+
+
+
+    //---------------------------------- 返回失败 -----------------------------------------
+
+    public static <T> JsonResult<T> resultError() {
+        return new JsonResult<T>(false, "网络异常");
+    }
+
+    public static <T> JsonResult<T> resultError(String message) {
+        return new JsonResult<T>(false, message);
+    }
+
+    public static <T> JsonResult<T> resultError(String message, String detail) {
+        return new JsonResult<T>(false, message, detail);
+    }
+
+    public static <T> JsonResult<T> resultError(String message, String detail, T obj) {
+        return new JsonResult<T>(false, message, detail, obj);
+    }
+
+    /**
+     * 当异常类型为BizException时，返回异常信息，否则返回默认信息
+     * 
+     * @param e
+     * @param defaultErrorMessage
+     * @return
+     */
+    public static <T> JsonResult<T> resultExceptionError(Exception e, String defaultErrorMessage) {
+    	if(e instanceof RuntimeException){
+    		return new JsonResult<T>(false, e.getMessage());
+    	}
+    	
+        return new JsonResult<T>(false, defaultErrorMessage);
+    }
+
+
+
+
+    /**----------------------------------- get set method -----------------------------------**/
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+    
+}
---
+++ example-microframe-common/src/main/java/com/example/microframe/common/result/RemoteResult.java
@@ -0,0 +1,134 @@
+package com.example.microframe.common.result;
+
+import java.io.Serializable;
+
+/**
+ * 封装json结果集
+ */
+public class RemoteResult<T>  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Integer code;          // 返回是否成功
+    private String msg = "";                  // 返回信提示，可用于前端显示
+    private String detail = "";               // 返回信息详情，保存不便于前端展示的内容
+    private T obj = null;                     // 返回其他对象信息
+
+    public RemoteResult() {
+    }
+
+    public RemoteResult(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public RemoteResult(Integer code, String msg, T obj) {
+        this.code = code;
+        this.msg = msg;
+        this.obj = obj;
+    }
+
+    public RemoteResult(Integer code, String msg, String detail) {
+        this.code = code;
+        this.msg = msg;
+        this.detail = detail;
+    }
+
+    public RemoteResult(Integer code, String msg, String detail, T obj) {
+        this.code = code;
+        this.msg = msg;
+        this.detail = detail;
+        this.obj = obj;
+    }
+
+    //---------------------------------- 返回成功 -----------------------------------------
+
+    public static <T> RemoteResult<T> resultcode() {
+        return resultcode("操作成功", null);
+    }
+
+    public static <T> RemoteResult<T> resultcode(T obj) {
+        return resultcode("操作成功", obj);
+    }
+
+    public static <T> RemoteResult<T> resultcode(String message, T obj) {
+        return new RemoteResult<T>(200, message, obj);
+    }
+
+    public static <T> RemoteResult<T> resultcode(String message, String detail, T obj) {
+        return new RemoteResult<T>(200, message, detail, obj);
+    }
+
+
+
+    //---------------------------------- 返回失败 -----------------------------------------
+
+    public static <T> RemoteResult<T> resultError() {
+        return new RemoteResult<T>(500, "网络异常");
+    }
+
+    public static <T> RemoteResult<T> resultError(String message) {
+        return new RemoteResult<T>(500, message);
+    }
+
+    public static <T> RemoteResult<T> resultError(String message, String detail) {
+        return new RemoteResult<T>(500, message, detail);
+    }
+
+    public static <T> RemoteResult<T> resultError(String message, String detail, T obj) {
+        return new RemoteResult<T>(500, message, detail, obj);
+    }
+
+    /**
+     * 当异常类型为BizException时，返回异常信息，否则返回默认信息
+     *
+     * @param e
+     * @param defaultErrorMessage
+     * @return
+     */
+    public static <T> RemoteResult<T> resultExceptionError(Exception e, String defaultErrorMessage) {
+        if(e instanceof RuntimeException){
+            return new RemoteResult<T>(500, e.getMessage());
+        }
+
+        return new RemoteResult<T>(500, defaultErrorMessage);
+    }
+
+
+
+
+    /**----------------------------------- get set method -----------------------------------**/
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public T getObj() {
+        return obj;
+    }
+
+    public void setObj(T obj) {
+        this.obj = obj;
+    }
+
+}
